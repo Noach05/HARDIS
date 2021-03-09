a3f9edb739674b6789738a8d92f36e61ddbea98d
TheArkNoah05 <55030669+Noach05@users.noreply.github.com>
2021-03-09 20:34:28 +0100

    Fixed the fuck-up

---
+++ src/main/java/thearknoah05/hardis/HardisBase.java
@@ -0,0 +1,33 @@
+package thearknoah05.hardis;
+
+
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import thearknoah05.hardis.itemgroups.HardisItemGroup;
+
+
+@Mod("hardisbase")
+public class HardisBase {
+
+    public static final String MODID = "hardisbase";
+    public static final Logger LOGGER = LogManager.getLogger();
+    public static final ItemGroup hardis = new HardisItemGroup();
+
+    public HardisBase() {
+        FMLJavaModLoadingContext.get().getModEventBus().register(this);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+        LOGGER.info("[HARDIS Base] Setup method registered");
+    }
+
+}
