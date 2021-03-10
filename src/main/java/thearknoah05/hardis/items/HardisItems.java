0fa8cbcef1e76323bf3dccb108809833dfe2ec51
TheArkNoah05 <55030669+Noach05@users.noreply.github.com>
2021-03-10 16:16:04 +0100

    Added in Item Recipes

--- src/main/java/thearknoah05/hardis/items/HardisItems.java
+++ src/main/java/thearknoah05/hardis/items/HardisItems.java
@@ -4,7 +4,7 @@ import net.minecraft.item.Item;
 
 public class HardisItems {
 
-    public static  Item tardis_heart;
+    public static Item tardis_heart;
     public static Item tardis_activator;
     public static Item time_vector_generator;
     public static Item relative_dimension_stabiliser;
